d43f27bf243e8b516e8f92cbe42043cf755f0ca9
Bárbara Este <barbaraeste01@gmail.com>
2019-09-15 14:02:36 -0300

    Dynamic allocation using malloc and free

--- dynamic-hanoi.c
+++ dynamic-hanoi.c
@@ -22,29 +22,50 @@ void naiveHanoi(int n, char src, char dest, char temp) {
 // três torres duas a duas).
 
 //Para qualquer [i][j], com 0 <= i < n+1 e j pertencente a
-//{'src->dest', 'src->temp', 'dest->src', 'dest->temp', 'temp->src', 'temp->dest'},
+//['src->dest', 'src->temp', 'dest->src', 'dest->temp', 'temp->src', 'temp->dest'],
 //0 indica que não se tem calculado a resposta para o disco n
 //no movimento j.
 
-void auxDynamicHanoi(char memoMatrix[][6], int n, int index, char *src, char *dest, char *temp) {
-	if(n == 0) return; //Caso base: quando n = 0, nenhum disco está sendo movido.
+char *auxDynamicHanoi(char memoMatrix[][6][100], int n, int index, char *src, char *dest, char *temp) {
+	int i;
+	if(n == 0) return ""; //Caso base: quando n = 0, nenhum disco está sendo movido.
 	
-	if(memoMatrix[n][index] == 0) {
+	if(memoMatrix[n][index][0] == 0) {
 		char *movimento = malloc(2 * sizeof(char));
 		strcat(movimento, dest);
 		strcat(movimento, src);
+		
+		char *temp1 = auxDynamicHanoi(memoMatrix, n-1, 1, src, temp, dest);
+		char *temp2 = auxDynamicHanoi(memoMatrix, n-1, 5, temp, dest, src);
+		
+		char *temp3 = malloc(sizeof(movimento) + sizeof(temp1));
+		strcat(temp3, movimento);
+		strcat(temp3, temp2);
+		
+		free(movimento);
+		free(temp1);
+		free(temp2);
+		
+		char *temp4 = malloc(sizeof(temp1) + sizeof(temp3));
+		strcat(temp4, temp1);
+		strcat(temp4, temp3); //Aqui o pointeiro temp4 contém o endereço para o resultado de n discos
 
-		char *resultado =  malloc(sizeof(strcat(auxDynamicHanoi(memoMatrix, n-1, index, src, temp, dest), strcat(movimento, auxDynamicHanoi(memoMatrix, n-1, index, temp, dest, src)))));
-		resultado = strcat(auxDynamicHanoi(memoMatrix, n-1, index, src, temp, dest), strcat(movimento, auxDynamicHanoi(memoMatrix, n-1, index, temp, dest, src)));
-		memoMatrix[n][index] = resultado;
+		memoMatrix[n][index][0] = malloc(sizeof(temp4));
+		memoMatrix[n][index][0] = temp4;
+		
+		free(temp3);
+		free(temp4);
 	}
-	return memoMatrix[n][index];
+
+	for(i = 0; i < 100; i++) {
+		printf(" %c ", memoMatrix[n][index][i]);
+	}
+	return memoMatrix[n][index][0];
 }
 
 void dynamicHanoi(int n, char src, char dest, char temp) {
-	char memoMatrix[n+1][6] = {{0}}; //Inicializando a matriz de memoização com zeros
-	
-	
+	char memoMatrix[n+1][6][100]; //Inicializando a matriz de memoização
+	auxDynamicHanoi(memoMatrix, n, 0, src, dest, temp);
 }
 
 int main() {
@@ -54,5 +75,6 @@ int main() {
 	printf("\nNro de discos = ");
 	scanf(" %d", &n);
 	
-	naiveHanoi(n, src, dest, temp);
+//	naiveHanoi(n, src, dest, temp);
+	dynamicHanoi(n, src, dest, temp);
 }
